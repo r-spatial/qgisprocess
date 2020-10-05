4777c717a7aad034c9488de7f1a20ec2d4222dbd
Jan Caha <jan.caha@outlook.com>
2020-10-05 20:30:05 +0200

    minor improvements to passing data to QGIS

--- R/qgis-arguments.R
+++ R/qgis-arguments.R
@@ -46,7 +46,7 @@ as_qgis_argument.sf <- function(x, qgis_type) {
     stop(glue::glue("Can't use 'sf' objects for QGIS arguments with type '{ qgis_type }'"), call. = FALSE)
   }
 
-  path <- tempfile(fileext = ".shp")
+  path <- tempfile(fileext = ".gpkg")
   sf::write_sf(x, path)
   structure(path, class = "qgis_tempfile")
 }
@@ -56,6 +56,17 @@ as_qgis_argument.sf <- function(x, qgis_type) {
 as_qgis_argument.RasterLayer <- function(x, qgis_type) {
   if (qgis_type != "raster") {
     stop(glue::glue("Can't use 'RasterLayer' objects for QGIS arguments with type '{ qgis_type }'"), call. = FALSE)
+  }
+
+  if (x@file@name != ""){
+    file_ext <- stringr::str_match(a,
+                                   ".*\\.([a-zA-Z]+)$")[2]
+
+    file_ext <- stringr::str_to_lower(file_ext)
+
+    if (file_ext %in% c("grd", "asc", "sdat", "rst", "nc", "tif", "tiff", "gtiff", "envi", "bil", "img")){
+      return(x@file@name)
+    }
   }
 
   path <- tempfile(fileext = ".tif")
