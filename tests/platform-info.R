34e80e1e417acaf09d48a6669c4bd11fbed7cb05
Dewey Dunnington <dewey@fishandwhistle.net>
2020-10-07 20:39:56 -0300

    GHA setup with draft of mac installers

--- tests/platform-info.R
+++ tests/platform-info.R
@@ -3,9 +3,15 @@ library(qgisprocess)
 
 has_qgis()
 if (is_macos()) qgis_detect_macos()
+
 if (is_windows()) qgis_detect_windows()
 
 if (has_qgis()) qgis_version()
+
+if (has_qgis()) qgis_path()
+
 if (has_qgis()) qgis_providers()
+
 if (has_qgis()) qgis_algorithms()
+
 if (has_qgis()) qgis_algorithms()$algorithm
