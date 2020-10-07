02f9d444f42be9070acb6545f2092fb84751af0e
Dewey Dunnington <dewey@fishandwhistle.net>
2020-10-07 19:54:53 -0300

    better algorithm output

--- tests/platform-info.R
+++ tests/platform-info.R
@@ -8,4 +8,4 @@ if (is_windows()) qgis_detect_windows()
 if (has_qgis()) qgis_version()
 if (has_qgis()) qgis_providers()
 if (has_qgis()) qgis_algorithms()
-if (has_qgis()) as.data.frame(qgis_algorithms()[c("algorithm", "algorithm_title")])
+if (has_qgis()) qgis_algorithms()$algorithm
