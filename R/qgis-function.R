19d18179c50b7ac7e46f5bd7812cff8aa82e3134
Dewey Dunnington <dewey@fishandwhistle.net>
2020-10-10 22:29:08 -0300

    better function partial generator

--- R/qgis-function.R
+++ R/qgis-function.R
@@ -10,53 +10,99 @@
 #'
 #' @inheritParams qgis_run_algorithm
 #' @param .data Passed to the first input of `algorithm`.
+#' @param ... Default values to set when using [qgis_function()].
+#'   These values are evaluated once and immediately, so you shouldn't
+#'   call [qgis_tmp_file()] here.
 #'
-#' @return A function wrapping `qgis_run_algorithm(algorithm, ...)`.
 #' @export
 #'
 #' @examples
 #' if (has_qgis()) {
 #'   qgis_buffer <- qgis_function("native:buffer")
 #'   qgis_buffer(
-#'     system.file("longlake/longlake_depth.gpkg", package = "qgisprocess"),
+#'     system.file(
+#'       "longlake/longlake_depth.gpkg",
+#'       package = "qgisprocess"
+#'     ),
 #'     DISTANCE = 10
 #'   )
 #' }
 #'
 #' if (has_qgis()) {
 #'   qgis_pipe(
-#'     system.file("longlake/longlake_depth.gpkg", package = "qgisprocess"),
+#'     system.file(
+#'       "longlake/longlake_depth.gpkg",
+#'       package = "qgisprocess"
+#'     ),
 #'     "native:buffer",
 #'     DISTANCE = 10
 #'   )
 #' }
 #'
-qgis_function <- function(algorithm, .quiet = TRUE) {
+qgis_function <- function(algorithm, ...) {
   assert_qgis()
   assert_qgis_algorithm(algorithm)
 
-  dots <- rlang::list2(.quiet = .quiet)
   args <- qgis_arguments(algorithm)
-  arg_names <- c(args$name, "PROJECT_PATH", "ELIPSOID")
-  qgis_algorithm_args <- lapply(rlang::set_names(arg_names), rlang::sym)
-  qgis_fun_args <- lapply(
-    rlang::set_names(arg_names),
-    function(x) rlang::call2("qgis_default_value", .ns = "qgisprocess")
-  )
-  qgis_fun_args[c("PROJECT_PATH", "ELIPSOID")] <- list(NULL)
+  arg_names <- c(args$name, "PROJECT_PATH", "ELIPSOID", ".quiet")
 
+  # The dots are the default values and are not exposed as
+  # function arguments, and become the environment for the function
+  dots <- rlang::list2(...)
+  if (length(dots) > 0 && !rlang::is_named(dots)) {
+    abort("All default arguments specified in `qgis_function(...)` must be named.")
+  }
+
+  invalid_dot_names <- setdiff(names(dots), arg_names)
+  if (length(invalid_dot_names) > 0) {
+    abort(
+      paste0(
+        "All default arguments specified in `qgis_function(...)` must be valid input names.\n",
+        "Invalid names: ",
+        glue::glue_collapse(paste0("'", invalid_dot_names, "'"), ", ", last = " and "),
+        "\nValid names: ",
+        glue::glue_collapse(paste0("'", arg_names, "'"), ", ", last = " and ")
+      )
+    )
+  }
+
+  # this makes it possible to have prettier/shorter formals() that show up
+  # in GUI complete helpers
+  dots$qgis_default_value <- qgis_default_value
+
+  # these become the arguments to qgis_run_algorithm()
+  qgis_algorithm_args <- lapply(rlang::set_names(arg_names), rlang::sym)
+
+  # these become the formals to the returned function
+  # default values are not exposed
+  qgis_fun_args <- lapply(
+    rlang::set_names(setdiff(arg_names, names(dots))),
+    function(x) rlang::call2("qgis_default_value")
+  )
+
+  # the non-algorithm args (but args to qgis_run_algorithm()) are not
+  # qgis_default_value()
+  default_run_alg_args <- list(PROJECT_PATH = NULL, ELIPSOID = NULL, .quiet = TRUE)
+  qgis_fun_args[intersect(names(default_run_alg_args), names(qgis_fun_args))] <-
+    default_run_alg_args[intersect(names(default_run_alg_args), names(qgis_fun_args))]
+
+  # generate the call to qgis_run_algorithm()
   qgis_algorithm_call <- rlang::call2(
     "qgis_run_algorithm",
     algorithm,
     !!! qgis_algorithm_args,
-    !!! dots,
     .ns = "qgisprocess"
   )
 
+  # set up the function
   fun <- function() {}
-  formals(fun) <- c(qgis_fun_args, dots)
+  formals(fun) <- qgis_fun_args
   body(fun) <- qgis_algorithm_call
-  environment(fun) <- baseenv()
+
+  # set the function environment
+  dots_env <- as.environment(dots)
+  parent.env(dots_env) <- baseenv()
+  environment(fun) <- dots_env
 
   fun
 }
--- tests/testthat/test-qgis-function.R
+++ tests/testthat/test-qgis-function.R
@@ -2,9 +2,18 @@
 test_that("qgis_function() works", {
   skip_if_not(has_qgis())
 
+  expect_error(
+    qgis_function("native:buffer", NOT_AN_ARG = "value"),
+    "must be valid input names"
+  )
+  expect_error(
+    qgis_function("native:buffer", "no name"),
+    "must be named"
+  )
+
   qgis_buffer <- qgis_function("native:buffer")
   expect_is(qgis_buffer, "function")
-  expect_identical(environment(qgis_buffer), baseenv())
+  expect_identical(parent.env(environment(qgis_buffer)), baseenv())
   expect_true(rlang::is_call(body(qgis_buffer), "qgis_run_algorithm", ns = "qgisprocess"))
 
   buffer_args <- qgis_arguments("native:buffer")
@@ -13,14 +22,16 @@ test_that("qgis_function() works", {
     c(buffer_args$name, setdiff(names(formals(qgis_run_algorithm)), c("algorithm", "...")))
   )
 
-  result <- qgis_buffer(
-    system.file("longlake/longlake_depth.gpkg", package = "qgisprocess"),
-    DISTANCE = 100,
-    DISSOLVE = TRUE,
-    MITER_LIMIT = 2,
-    OUTPUT = qgis_tmp_vector(),
-    END_CAP_STYLE = 0,
-    JOIN_STYLE = 0
+  result <- expect_silent(
+    qgis_buffer(
+      system.file("longlake/longlake_depth.gpkg", package = "qgisprocess"),
+      DISTANCE = 100,
+      DISSOLVE = TRUE,
+      MITER_LIMIT = 2,
+      OUTPUT = qgis_tmp_vector(),
+      END_CAP_STYLE = 0,
+      JOIN_STYLE = 0
+    )
   )
 
   expect_is(result, "qgis_result")
