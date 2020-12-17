494ae6ca4ea21cf7363784003e4bd3b68034c676
jannes.muenchow <jannes.muenchow@gfk.com>
2020-12-17 14:49:14 +0100

    call sf functions explicitly

--- tests/testthat/test-qgis-algorithm.R
+++ tests/testthat/test-qgis-algorithm.R
@@ -34,17 +34,19 @@ test_that("qgis_run_algorithm() ignores unknown inputs", {
 
 test_that("qgis_run_algorithm accepts multipleinput arguments", {
   skip_if_not(has_qgis())
+  skip_if_not_installed("sf")
+
   v_1 = sf::read_sf(
     system.file("longlake/longlake.gpkg", package = "qgisprocess"))
   v_2 = v_3 = v_1
   v_2$geom = v_2$geom + 1000
-  st_crs(v_2) = st_crs(v_1)
+  sf::st_crs(v_2) = sf::st_crs(v_1)
   v_3$geom = v_3$geom - 1000
-  st_crs(v_3) = st_crs(v_1)
+  sf::st_crs(v_3) = sf::st_crs(v_1)
   out = qgis_run_algorithm("native:mergevectorlayers",
                            LAYERS = v_1, LAYERS = v_2, LAYERS = v_3,
                            .quiet = TRUE)
-  tmp = read_sf(qgis_output(out, "OUTPUT"))
+  tmp = sf::read_sf(qgis_output(out, "OUTPUT"))
   expect_equal(nrow(tmp), 3)
 })
 
