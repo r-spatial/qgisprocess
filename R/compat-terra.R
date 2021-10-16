b0ac2abaa9db7e3ddecb23e98ce2fdf9f7cfb940
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-10-16 19:37:43 +0200

    adds some terra templates

---
+++ R/compat-terra.R
@@ -0,0 +1,83 @@
+
+#' Convert raster objects to/from QGIS inputs/outputs
+#'
+#' @param x A [terra::rast()].
+#' @param output The result from [qgis_run_algorithm()] or [qgis_output()].
+#' @param ... Passed to [terra::rast()].
+#' @inheritParams as_qgis_argument
+#'
+#' @export
+#'
+as_qgis_argument.SpatRaster <- function(x, spec = qgis_argument_spec()) {
+  as_qgis_argument_terra(x, spec)
+}
+
+as_qgis_argument_terra <- function(x, spec = qgis_argument_spec()) {
+  if (!isTRUE(spec$qgis_type %in% c("raster", "layer", "multilayer"))) {
+    abort(glue("Can't convert '{ class(x)[1] }' object to QGIS type '{ spec$qgis_type }'"))
+  }
+
+  # try to use a filename if present
+  if (terra::sources(obj)$source != "") {
+    file_ext <- stringr::str_to_lower(tools::file_ext(terra::sources(obj)$source))
+    if (file_ext %in% c("grd", "asc", "sdat", "rst", "nc", "tif", "tiff", "gtiff", "envi", "bil", "img")) {
+      return(terra::sources(obj)$source)
+    }
+  }
+
+  path <- qgis_tmp_raster()
+  terra::writeRaster(x, path)
+  structure(path, class = "qgis_tempfile_arg")
+}
+
+#' @rdname as_qgis_argument.SpatRaster
+#' @export
+qgis_as_terra <- function(output, ...) {
+  UseMethod("qgis_as_raster")
+}
+
+#' @rdname as_qgis_argument.SpatRaster
+#' @export
+qgis_as_terra.qgis_outputRaster <- function(output, ...) {
+  terra::rast(unclass(output), ...)
+}
+
+#' @rdname as_qgis_argument.SpatRaster
+#' @export
+qgis_as_terra.qgis_outputLayer <- function(output, ...) {
+  terra::rast(unclass(output), ...)
+}
+
+#' @rdname as_qgis_argument.RasterLayer
+#' @export
+qgis_as_terra.qgis_result <- function(output, ...) {
+  # find the first raster output and read it
+  for (result in output) {
+    if (inherits(result, "qgis_outputRaster") || inherits(result, "qgis_outputLayer")) {
+      return(terra::rast(unclass(result), ...))
+    }
+  }
+
+  abort("Can't extract 'terra' raster from result: zero outputs of type 'outputRaster' or 'outputLayer'.")
+}
+
+
+#' @export
+as_qgis_argument.CRS <- function(x, spec = qgis_argument_spec()) {
+  if (!isTRUE(spec$qgis_type %in% c("crs"))) {
+    abort(glue("Can't convert 'crs' object to QGIS type '{ spec$qgis_type }'"))
+  }
+
+  raster::wkt(x)
+}
+
+#' @export
+as_qgis_argument.SpatExtent <- function(x, spec = qgis_argument_spec()) {
+  if (!isTRUE(spec$qgis_type %in% c("extent"))) {
+    abort(glue("Can't convert 'SpatExtent' object to QGIS type '{ spec$qgis_type }'"))
+  }
+
+  ex <- as.vector(bb)
+
+  glue("{ex[['xmin']]},{ex[['xmax']]},{ex[['ymin']]},{ex[['ymax']]}")
+}
---
+++ tests/testthat/test-compat-terra.R
@@ -0,0 +1,156 @@
+
+test_that("raster argument coercers work", {
+  skip_if_not_installed("terra")
+
+  obj <- terra::rast()
+  expect_error(
+    as_qgis_argument(obj),
+    "Can't convert 'RasterLayer' object"
+  )
+
+  tmp_file <- expect_match(
+    suppressWarnings(as_qgis_argument(obj, qgis_argument_spec(qgis_type = "layer"))),
+    "\\.tif$"
+  )
+  expect_is(tmp_file, "qgis_tempfile_arg")
+  unlink(tmp_file)
+
+  # also check rasters with embedded files
+  obj <- terra::rast(system.file("longlake/longlake.tif", package = "qgisprocess"))
+  expect_identical(
+    as_qgis_argument(obj, qgis_argument_spec(qgis_type = "layer")),
+    obj@file@name
+  )
+
+  # check RasterBrick
+  obj <- raster::brick(system.file("longlake/longlake.tif", package = "qgisprocess"))
+  expect_identical(
+    as_qgis_argument(obj, qgis_argument_spec(qgis_type = "layer")),
+    obj@file@name
+  )
+})
+
+test_that("raster result coercers work", {
+  skip_if_not_installed("terra")
+
+  expect_is(
+    qgis_as_raster(
+      structure(
+        system.file("longlake/longlake.tif", package = "qgisprocess"),
+        class = "qgis_outputRaster"
+      )
+    ),
+    "RasterLayer"
+  )
+
+  expect_is(
+    qgis_as_brick(
+      structure(
+        system.file("longlake/longlake.tif", package = "qgisprocess"),
+        class = "qgis_outputRaster"
+      )
+    ),
+    "RasterBrick"
+  )
+
+  expect_is(
+    qgis_as_raster(
+      structure(
+        system.file("longlake/longlake.tif", package = "qgisprocess"),
+        class = "qgis_outputLayer"
+      )
+    ),
+    "RasterLayer"
+  )
+
+  expect_is(
+    qgis_as_brick(
+      structure(
+        system.file("longlake/longlake.tif", package = "qgisprocess"),
+        class = "qgis_outputLayer"
+      )
+    ),
+    "RasterBrick"
+  )
+
+  expect_is(
+    qgis_as_raster(
+      structure(
+        list(
+          OUTPUT = structure(
+            system.file("longlake/longlake.tif", package = "qgisprocess"),
+            class = "qgis_outputRaster"
+          )
+        ),
+        class = "qgis_result"
+      )
+    ),
+    "RasterLayer"
+  )
+
+  expect_is(
+    qgis_as_brick(
+      structure(
+        list(
+          OUTPUT = structure(
+            system.file("longlake/longlake.tif", package = "qgisprocess"),
+            class = "qgis_outputRaster"
+          )
+        ),
+        class = "qgis_result"
+      )
+    ),
+    "RasterBrick"
+  )
+})
+
+test_that("raster crs work", {
+  skip_if_not_installed("terra")
+
+  obj <- raster::raster(system.file("longlake/longlake.tif", package = "qgisprocess"))
+
+  crs_representation <- expect_match(
+    as_qgis_argument(raster::crs(obj), qgis_argument_spec(qgis_type = "crs")),
+    "UTM zone 20N"
+  )
+
+  expect_is(crs_representation, "character")
+})
+
+test_that("raster argument coercer for extent works", {
+  skip_if_not_installed("terra")
+
+  obj <- raster::raster(system.file("longlake/longlake.tif", package = "qgisprocess"))
+
+
+  bbox_representation <- expect_match(
+    as_qgis_argument(raster::extent(obj), qgis_argument_spec(qgis_type = "extent")),
+    "409891\\.446955431,411732\\.936955431,5083288\\.89932423,5084852\\.61932423"
+  )
+
+  expect_is(bbox_representation, "character")
+})
+
+test_that("raster argument coercer for crs works", {
+  skip_if_not_installed("terra")
+  skip_if_not(has_qgis())
+
+  # Until Issue #36 is resolved (WKT2 as an argument causes a failure)
+  skip_on_os("windows")
+
+  obj <- raster::raster(system.file("longlake/longlake.tif", package = "qgisprocess"))
+
+  result <- qgis_run_algorithm(
+    "native:createconstantrasterlayer",
+    EXTENT = raster::extent(obj),
+    TARGET_CRS = raster::crs(obj),
+    PIXEL_SIZE = 1000,
+    OUTPUT_TYPE = "Byte",
+    OUTPUT = qgis_tmp_raster(),
+    NUMBER = 5,
+    .quiet = TRUE
+  )
+
+  expect_true(file.exists(result$OUTPUT))
+  qgis_result_clean(result)
+})
