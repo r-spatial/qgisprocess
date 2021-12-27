3376b0911188e5ad5c0f811f36305bd6e8bbfbc4
Dewey Dunnington <dewey@fishandwhistle.net>
2021-12-26 21:16:53 -0400

    use json output

--- R/qgis-output.R
+++ R/qgis-output.R
@@ -1,30 +1,42 @@
 
-# I wish this were less of a hack - this is probably subject to
-# bugs related to encodings and special characters in output strings
 qgis_parse_results <- function(algorithm, output) {
-  sec_results <- stringr::str_match(
-    output,
-    stringr::regex(
-      "-+\\s+Results\\s+-+\\s+(.*)",
-      dotall = TRUE, multiline = TRUE
+  if (stringr::str_detect(output, "^\\s*\\{")) {
+    output_parsed <- jsonlite::fromJSON(output)
+    outputs_list <- output_parsed$results
+    output_names <- names(outputs_list)
+
+    algorithm_outputs <- qgis_outputs(algorithm)
+
+    Map(
+      qgis_result_output,
+      outputs_list,
+      algorithm_outputs$qgis_output_type[match(output_names, algorithm_outputs$name)]
     )
-  )[, 2, drop = TRUE]
+  } else {
+    sec_results <- stringr::str_match(
+      output,
+      stringr::regex(
+        "-+\\s+Results\\s+-+\\s+(.*)",
+        dotall = TRUE, multiline = TRUE
+      )
+    )[, 2, drop = TRUE]
 
-  output_lines <- readLines(textConnection(trimws(sec_results)))
-  outputs <- stringr::str_split(output_lines, "\\s*:\\s*", n = 2)
-  outputs_list <- lapply(outputs, "[", 2)
-  output_names <- vapply(outputs, "[", 1, FUN.VALUE = character(1))
+    output_lines <- readLines(textConnection(trimws(sec_results)))
+    outputs <- stringr::str_split(output_lines, "\\s*:\\s*", n = 2)
+    outputs_list <- lapply(outputs, "[", 2)
+    output_names <- vapply(outputs, "[", 1, FUN.VALUE = character(1))
 
-  algorithm_outputs <- qgis_outputs(algorithm)
+    algorithm_outputs <- qgis_outputs(algorithm)
 
-  outputs_list <- Map(
-    qgis_parse_result_output,
-    outputs_list,
-    algorithm_outputs$qgis_output_type[match(output_names, algorithm_outputs$name)]
-  )
+    outputs_list <- Map(
+      qgis_parse_result_output,
+      outputs_list,
+      algorithm_outputs$qgis_output_type[match(output_names, algorithm_outputs$name)]
+    )
 
-  names(outputs_list) <- output_names
-  outputs_list
+    names(outputs_list) <- output_names
+    outputs_list
+  }
 }
 
 # All values of `qgis_output_type`
@@ -49,8 +61,22 @@ qgis_parse_result_output <- function(value, qgis_output_type) {
       structure(stringr::str_split(value, "\\s*,\\s*")[[1]], class = "qgis_outputMultilayer")
     },
 
-    # by default, a classed string that can be reinterpreted by
-    # various functions
+    # by default, a classed string that can be reinterpreted
+    structure(value, class = paste0("qgis_", qgis_output_type))
+  )
+}
+
+qgis_result_output <- function(value, qgis_output_type) {
+  switch(
+    qgis_output_type,
+
+    # numbers and strings have clear mappings to R types
+    outputNumber = as.numeric(value),
+    outputString = as.character(value),
+
+    outputMultilayer = structure(as.character(value), class = paste0("qgis_", qgis_output_type)),
+
+    # by default, a classed string that can be reinterpreted
     structure(value, class = paste0("qgis_", qgis_output_type))
   )
 }
