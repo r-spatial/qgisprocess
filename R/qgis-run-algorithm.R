bd228f3535a3ebe644397601f97a231f5e62a5a5
Dewey Dunnington <dewey@fishandwhistle.net>
2022-01-10 07:28:37 -0400

    allow raw JSON as input

--- R/qgis-run-algorithm.R
+++ R/qgis-run-algorithm.R
@@ -14,6 +14,7 @@
 #'   [rlang::list2()] are supported. These arguments
 #'   are converted to strings using [as_qgis_argument()].
 #' @param .quiet Use `TRUE` to suppress output from processing algorithms.
+#' @param .raw_json_input The raw JSON to use as input in place of `...`.
 #'
 #' @export
 #'
@@ -26,7 +27,8 @@
 #'   )
 #' }
 #'
-qgis_run_algorithm <- function(algorithm, ..., PROJECT_PATH = NULL, ELLIPSOID = NULL, .quiet = FALSE) {
+qgis_run_algorithm <- function(algorithm, ..., PROJECT_PATH = NULL, ELLIPSOID = NULL,
+                               .raw_json_input = NULL, .quiet = FALSE) {
   assert_qgis()
   assert_qgis_algorithm(algorithm)
 
@@ -35,21 +37,30 @@ qgis_run_algorithm <- function(algorithm, ..., PROJECT_PATH = NULL, ELLIPSOID = 
     abort("All ... arguments to `qgis_run_algorithm()` must be named.")
   }
 
-  use_json_input <- qgis_use_json_input()
+  use_json_input <- !is.null(.raw_json_input) || qgis_use_json_input()
   use_json_output <- use_json_input || qgis_use_json_output()
 
-  # sanitize arguments and make sure they are cleaned up on exit
-  args <- qgis_sanitize_arguments(
-    algorithm,
-    !!! dots,
-    PROJECT_PATH = PROJECT_PATH,
-    ELLIPSOID = ELLIPSOID,
-    .use_json_input = use_json_input
-  )
-  on.exit(qgis_clean_arguments(args))
+  if (is.null(.raw_json_input)) {
+    # sanitize arguments and make sure they are cleaned up on exit
+    args <- qgis_sanitize_arguments(
+      algorithm,
+      !!! dots,
+      PROJECT_PATH = PROJECT_PATH,
+      ELLIPSOID = ELLIPSOID,
+      .use_json_input = use_json_input
+    )
+    on.exit(qgis_clean_arguments(args))
 
-  # generate command-line args or JSON input
-  args_str <- qgis_serialize_arguments(args, use_json_input = use_json_input)
+    # generate command-line args or JSON input
+    args_str <- qgis_serialize_arguments(args, use_json_input = use_json_input)
+  } else {
+    args_str <- .raw_json_input
+    args <- list()
+    if (length(list(...)) > 0) {
+      abort("Can't use `.json_input` with arguments in `qgis_run_algorithm()`")
+    }
+  }
+
   if (use_json_input) {
     stdin_file <- tempfile()
     on.exit(unlink(stdin_file), add = TRUE)
@@ -85,6 +96,7 @@ qgis_run_algorithm <- function(algorithm, ..., PROJECT_PATH = NULL, ELLIPSOID = 
       !!! qgis_parse_results(algorithm, result$stdout),
       .algorithm = algorithm,
       .args = args,
+      .raw_json_input = if (use_json_input) args_str,
       .processx_result = result
     ),
     class = "qgis_result"
