2ee0aceb28c1c57a45ba236c45e044a7098c2055
florisvdh <floris.vanderhaeghe@inbo.be>
2022-09-02 09:47:40 +0200

    test-qgis-run-algorithm2.R: only setup if conditions fulfilled *
    Otherwise failure occurs on test systems without QGIS, when doing the withr-setup

--- tests/testthat/test-qgis-run-algorithm2.R
+++ tests/testthat/test-qgis-run-algorithm2.R
@@ -1,9 +1,9 @@
 # Flip qgis_use_json_input() and rerun test-qgis-run-algorithm.R
 
+if (has_qgis() && package_version(strsplit(qgis_version(), "-")[[1]][1]) >= "3.23.0") {
+
 withr::local_envvar(c(JSON_INPUT = qgis_use_json_input()))
 withr::local_options(qgisprocess.use_json_input = !qgis_use_json_input())
-
-if (has_qgis() && package_version(strsplit(qgis_version(), "-")[[1]][1]) >= "3.23.0") {
 
   test_that("Flipping JSON input method before re-testing works", {
     expect_identical(
