a815be2143cd61a430fe662726d7a2afe51af792
florisvdh <floris.vanderhaeghe@inbo.be>
2023-02-24 09:55:57 +0100

    tests/platform-info.R: extend reported info

--- tests/platform-info.R
+++ tests/platform-info.R
@@ -4,13 +4,22 @@ library(qgisprocess)
 qgis_configure(use_cached_data = TRUE) # triggers reconfiguration if needed
 
 has_qgis()
+
 if (is_macos()) qgis_detect_macos()
 if (is_windows()) qgis_detect_windows()
 
 if (has_qgis()) qgis_path()
 if (has_qgis()) qgis_version()
+
+if (has_qgis()) qgis_use_json_output()
+if (has_qgis()) qgis_use_json_input()
+
 if (has_qgis()) cat(qgis_run()$stdout)
 if (has_qgis()) cat(qgis_run()$stderr)
+
+if (has_qgis()) cat(qgis_run(args = "--version")$stdout)
+
+if (has_qgis()) qgis_plugins()
 
 if (has_qgis()) qgis_providers()
 
