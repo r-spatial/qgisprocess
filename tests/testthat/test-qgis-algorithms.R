48db885eaf7b53c4ba139142967283da3827a6db
florisvdh <floris.vanderhaeghe@inbo.be>
2023-03-01 18:46:45 +0100

    test-qgis-algorithms.R: add expectations for qgis_{algorithms,providers}()

--- tests/testthat/test-qgis-algorithms.R
+++ tests/testthat/test-qgis-algorithms.R
@@ -1,3 +1,10 @@
+test_that("qgis_algorithms() works", {
+  skip_if_not(has_qgis())
+  expect_s3_class(qgis_algorithms(), "data.frame")
+  expect_gt(nrow(qgis_algorithms()), 200)
+  expect_gt(ncol(qgis_algorithms()), 20)
+})
+
 test_that("qgis_has_algorithm() works", {
   skip_if_not(has_qgis())
   expect_true(qgis_has_algorithm("native:filedownloader"))
@@ -12,8 +19,10 @@ test_that("qgis_has_provider() works", {
 
 test_that("qgis_providers() works", {
   skip_if_not(has_qgis())
+  expect_s3_class(qgis_providers(), "data.frame")
   expect_true("native" %in% qgis_providers()$provider)
   expect_false("notaprovider" %in% qgis_providers()$provider)
+  expect_identical(ncol(qgis_providers()), 3L)
 })
 
 test_that("assert_qgis_algorithm() works", {
