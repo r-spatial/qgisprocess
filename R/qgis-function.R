68de4d05105257e5b17dd6c081d97b0593f7f8fe
florisvdh <floris.vanderhaeghe@inbo.be>
2023-03-08 11:36:03 +0100

    qgis_pipe(): also accept a qgis_result object

--- R/qgis-function.R
+++ R/qgis-function.R
@@ -109,6 +109,24 @@ qgis_function <- function(algorithm, ...) {
 #' @rdname qgis_function
 #' @export
 qgis_pipe <- function(.data, algorithm, ..., .quiet = TRUE) {
+  if (inherits(.data, "qgis_result")) {
+    withr::with_options(
+      list(warning.length = 6e3),
+      assert_that(
+        any(grepl("^(output|OUTPUT)$", names(.data))),
+        msg = paste0(
+          "The qgis_result object misses an 'OUTPUT' element; ",
+          "hence piping is not supported.\n",
+          "You can extract the needed output element (see `?qgis_output()`) ",
+          "and pipe that instead.\n",
+          "The included JSON-output was:\n",
+          jsonlite::prettify(.data$.processx_result$stdout)
+        )
+      )
+    )
+    # take the first OUTPUT or output element of .data and coerce to character:
+    .data <- as.character(.data[grepl("^(output|OUTPUT)$", names(.data))][1][[1]])
+  }
   fun <- qgis_function(algorithm)
   fun(.data, ..., .quiet = .quiet)
 }
