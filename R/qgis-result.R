3d7d74f927ed3cd89f28d3c0b07efe164dccf6d7
Jan Caha <jan.caha@outlook.com>
2023-03-21 22:07:16 +0100

    qgis_extract_output_* functions

--- R/qgis-result.R
+++ R/qgis-result.R
@@ -17,6 +17,7 @@ is_qgis_result <- function(x) {
   inherits(x, "qgis_result")
 }
 
+
 #' @rdname is_qgis_result
 #' @export
 qgis_result_clean <- function(x) {
@@ -25,17 +26,66 @@ qgis_result_clean <- function(x) {
   invisible(x)
 }
 
+
 #' @rdname is_qgis_result
 #' @export
-qgis_output <- function(x, which, default = qgis_error_output_does_not_exist(x, which)) {
-  if (is.numeric(which) && (which %in% seq_along(x))) {
-    x[[which]]
-  } else if (which %in% names(x)) {
+#'
+qgis_extract_output_by_name <- function(x, which = "OUTPUT") {
+  if (which %in% names(x)) {
     x[[which]]
   } else {
-    default
+    abort(
+      qgis_error_output_does_not_exist(x, which)
+    )
   }
 }
+
+
+#' @rdname is_qgis_result
+#' @export
+#'
+qgis_extract_output_by_position <- function(x, which) {
+  if (is.numeric(which) && (which %in% seq_along(x))) {
+    x[[which]]
+  } else {
+    abort(
+      qgis_error_output_does_not_exist(x, which)
+    )
+  }
+}
+
+
+#' @rdname is_qgis_result
+#' @export
+qgis_extract_output_by_class <- function(x, what) {
+  # Limit result to elements that match class
+  x <- x[vapply(x, inherits, what, FUN.VALUE = logical(1))]
+  if (length(x) == 0L) {
+    abort(
+      paste(
+        "Can't extract object from result: zero outputs of type",
+        paste(what, collapse = " or ")
+      )
+    )
+  }
+
+  # By default, take the first element named as output or OUTPUT.
+  # Otherwise, take the first element that matches class.
+  result <- x[grepl("^(output|OUTPUT)$", names(x))][1][[1]]
+  if (is.null(result)) result <- x[[1]]
+  result
+}
+
+
+#' @rdname is_qgis_result
+#' @export
+qgis_result_single <- function(x) {
+  # Limit result to elements that match class
+  result <- x[grepl("^(output|OUTPUT)$", names(x))][1][[1]]
+  if (is.null(result)) result <- x[[1]]
+  result
+}
+
 
 #' @rdname is_qgis_result
 #' @export
@@ -60,31 +110,6 @@ qgis_check_stdout <- function(x) {
     )
   }
 }
-
-
-#' @rdname is_qgis_result
-#' @export
-qgis_result_single <- function(x, what) {
-  # Limit result to elements that match class
-  if (!missing(what)) {
-    x <- x[vapply(x, inherits, what, FUN.VALUE = logical(1))]
-    if (length(x) == 0L) {
-      abort(
-        paste(
-          "Can't extract object from result: zero outputs of type",
-          paste(what, collapse = " or ")
-        )
-      )
-    }
-  }
-
-  # By default, take the first element named as output or OUTPUT.
-  # Otherwise, take the first element that matches class.
-  result <- x[grepl("^(output|OUTPUT)$", names(x))][1][[1]]
-  if (is.null(result)) result <- x[[1]]
-  result
-}
-
 
 
 #' @rdname is_qgis_result
--- tests/testthat/test-qgis-result.R
+++ tests/testthat/test-qgis-result.R
@@ -1,7 +1,6 @@
 test_that("qgis_output() works", {
-  expect_identical(qgis_output(list(a = 1), "a"), 1)
-  expect_identical(qgis_output(list(a = 1), "b", default = 2), 2)
-  expect_error(qgis_output(list(a = 1), "b"), "Result has no output")
+  expect_identical(qgis_extract_output_by_name(list(a = 1), "a"), 1)
+  expect_error(qgis_extract_output_by_name(list(a = 1), "b"), "Result has no output")
 })
 
 test_that("qgis_result_*() functions work", {
@@ -35,16 +34,137 @@ test_that("qgis_result_*() functions work", {
   expect_identical(qgis_result_status(result), 0L)
   expect_type(qgis_result_stderr(result), "character")
   expect_type(qgis_result_stdout(result), "character")
-  expect_error(qgis_result_single(result, "numeric"), "zero outputs of type")
+  expect_error(qgis_extract_output_by_class(result, "numeric"), "zero outputs of type")
+
+  result$.processx_result$stdout <- ""
+  expect_error(qgis_check_stdout(result), "output could not be captured")
+})
+
+test_that("qgis_extract_output_by_name() functions work", {
+  skip_if_not(has_qgis())
+
+  tmp_gpkg <- qgis_tmp_vector(".gpkg")
+  tmp_gpkg2 <- qgis_tmp_vector(".gpkg")
+
+  result <- qgis_run_algorithm(
+    "native:extractbyattribute",
+    INPUT = system.file("longlake/longlake_depth.gpkg", package = "qgisprocess"),
+    FIELD = "WAYPOINT_I",
+    OPERATOR = 0,
+    VALUE = "10",
+    OUTPUT = tmp_gpkg,
+    FAIL_OUTPUT = tmp_gpkg2,
+    .quiet = TRUE
+  )
+
+  expect_true(all(file.exists(tmp_gpkg, tmp_gpkg2)))
+  qgis_result_clean(result)
+  expect_false(any(file.exists(tmp_gpkg, tmp_gpkg2)))
+
   expect_identical(
-    qgis_result_single(result, "qgis_outputVector"),
+    qgis_extract_output_by_name(result, "OUTPUT"),
     result$OUTPUT
   )
+  expect_identical(
+    qgis_extract_output_by_name(result, "FAIL_OUTPUT"),
+    result$FAIL_OUTPUT
+  )
+  expect_identical(
+    qgis_extract_output_by_name(result),
+    result$OUTPUT
+  )
+
+  expect_error(qgis_extract_output_by_name(result, "A"), "Result has no output")
+  expect_error(qgis_extract_output_by_name(result, 1), "Result has no output")
+})
+
+test_that("qgis_extract_output_by_position() functions work", {
+  skip_if_not(has_qgis())
+
+  tmp_gpkg <- qgis_tmp_vector(".gpkg")
+  tmp_gpkg2 <- qgis_tmp_vector(".gpkg")
+
+  result <- qgis_run_algorithm(
+    "native:extractbyattribute",
+    INPUT = system.file("longlake/longlake_depth.gpkg", package = "qgisprocess"),
+    FIELD = "WAYPOINT_I",
+    OPERATOR = 0,
+    VALUE = "10",
+    OUTPUT = tmp_gpkg,
+    FAIL_OUTPUT = tmp_gpkg2,
+    .quiet = TRUE
+  )
+
+  expect_true(all(file.exists(tmp_gpkg, tmp_gpkg2)))
+  qgis_result_clean(result)
+  expect_false(any(file.exists(tmp_gpkg, tmp_gpkg2)))
+
+  expect_identical(
+    qgis_extract_output_by_position(result, 1),
+    result$FAIL_OUTPUT
+  )
+  expect_identical(
+    qgis_extract_output_by_position(result, 2),
+    result$OUTPUT
+  )
+
+  expect_error(qgis_extract_output_by_position(result, "A"), "Result has no output")
+  expect_error(qgis_extract_output_by_position(result, 8), "Result has no output")
+})
+
+test_that("qgis_extract_output_by_class() functions work", {
+  skip_if_not(has_qgis())
+
+  tmp_gpkg <- qgis_tmp_vector(".gpkg")
+  tmp_gpkg2 <- qgis_tmp_vector(".gpkg")
+
+  result <- qgis_run_algorithm(
+    "native:extractbyattribute",
+    INPUT = system.file("longlake/longlake_depth.gpkg", package = "qgisprocess"),
+    FIELD = "WAYPOINT_I",
+    OPERATOR = 0,
+    VALUE = "10",
+    OUTPUT = tmp_gpkg,
+    FAIL_OUTPUT = tmp_gpkg2,
+    .quiet = TRUE
+  )
+
+  expect_true(all(file.exists(tmp_gpkg, tmp_gpkg2)))
+  qgis_result_clean(result)
+  expect_false(any(file.exists(tmp_gpkg, tmp_gpkg2)))
+
+  expect_identical(
+    qgis_extract_output_by_class(result, "qgis_outputVector"),
+    result$OUTPUT
+  )
+
+  expect_error(qgis_extract_output_by_class(result, "A"), "Can't extract object")
+  expect_error(qgis_extract_output_by_class(result, 1), "must be a character vector")
+})
+
+test_that("qgis_result_single() functions work", {
+  skip_if_not(has_qgis())
+
+  tmp_gpkg <- qgis_tmp_vector(".gpkg")
+  tmp_gpkg2 <- qgis_tmp_vector(".gpkg")
+
+  result <- qgis_run_algorithm(
+    "native:extractbyattribute",
+    INPUT = system.file("longlake/longlake_depth.gpkg", package = "qgisprocess"),
+    FIELD = "WAYPOINT_I",
+    OPERATOR = 0,
+    VALUE = "10",
+    OUTPUT = tmp_gpkg,
+    FAIL_OUTPUT = tmp_gpkg2,
+    .quiet = TRUE
+  )
+
+  expect_true(all(file.exists(tmp_gpkg, tmp_gpkg2)))
+  qgis_result_clean(result)
+  expect_false(any(file.exists(tmp_gpkg, tmp_gpkg2)))
+
   expect_identical(
     qgis_result_single(result),
     result$OUTPUT
   )
-
-  result$.processx_result$stdout <- ""
-  expect_error(qgis_check_stdout(result), "output could not be captured")
 })
