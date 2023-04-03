81e6b50234904a2d8cd0f25cea400d06da226b83
JanCaha <jan.caha@outlook.com>
2023-04-03 13:44:12 +0200

    update tests

--- tests/testthat/test-qgis-result.R
+++ tests/testthat/test-qgis-result.R
@@ -1,6 +1,11 @@
-test_that("qgis_output() works", {
+test_that("qgis_extract_output_by_name() works", {
   expect_identical(qgis_extract_output_by_name(list(a = 1), "a"), 1)
   expect_error(qgis_extract_output_by_name(list(a = 1), "b"), "Result has no output")
+  expect_identical(qgis_extract_output_by_name(list(a = 1, output = 5)), 5)
+  expect_identical(qgis_extract_output_by_name(list(a = 1, notoutput = 8)), 1)
+
+  expect_error(qgis_extract_output_by_name(list(a = 1, notoutput = 8), single = FALSE), "Result has no output")
+  expect_error(qgis_extract_output_by_name(list(a = 1, notoutput = 8), "b", single = FALSE), "Result has no output")
 })
 
 test_that("qgis_result_*() functions work", {
@@ -74,7 +79,7 @@ test_that("qgis_extract_output_by_name() functions work", {
     result$OUTPUT
   )
 
-  expect_error(qgis_extract_output_by_name(result, "A"), "Result has no output")
+  expect_error(qgis_extract_output_by_name(result, "A" ), "Result has no output")
   expect_error(qgis_extract_output_by_name(result, 1), "Result has no output")
 })
 
@@ -140,31 +145,4 @@ test_that("qgis_extract_output_by_class() functions work", {
 
   expect_error(qgis_extract_output_by_class(result, "A"), "Can't extract object")
   expect_error(qgis_extract_output_by_class(result, 1), "must be a character vector")
-})
-
-test_that("qgis_result_single() functions work", {
-  skip_if_not(has_qgis())
-
-  tmp_gpkg <- qgis_tmp_vector(".gpkg")
-  tmp_gpkg2 <- qgis_tmp_vector(".gpkg")
-
-  result <- qgis_run_algorithm(
-    "native:extractbyattribute",
-    INPUT = system.file("longlake/longlake_depth.gpkg", package = "qgisprocess"),
-    FIELD = "WAYPOINT_I",
-    OPERATOR = 0,
-    VALUE = "10",
-    OUTPUT = tmp_gpkg,
-    FAIL_OUTPUT = tmp_gpkg2,
-    .quiet = TRUE
-  )
-
-  expect_true(all(file.exists(tmp_gpkg, tmp_gpkg2)))
-  qgis_result_clean(result)
-  expect_false(any(file.exists(tmp_gpkg, tmp_gpkg2)))
-
-  expect_identical(
-    qgis_result_single(result),
-    result$OUTPUT
-  )
 })
