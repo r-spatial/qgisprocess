17270eb1a77a33acbf05af6bbdc15d402b5c2428
florisvdh <floris.vanderhaeghe@inbo.be>
2023-05-08 11:04:30 +0200

    qgis_result_single(): don't yet drop, but deprecate for some time

--- R/qgisprocess-deprecated.R
+++ R/qgisprocess-deprecated.R
@@ -2,9 +2,13 @@
 #'
 #' - Instead of `qgis_output()`, use [qgis_extract_output()] and related
 #' functions.
+#' - Instead of `qgis_result_single()`, use [qgis_extract_output()] and related
+#' functions.
 #'
 #'
 #' @inheritParams is_qgis_result
+#' @param what Character vector of classes.
+#'
 #' @name qgisprocess-deprecated
 
 #' @rdname qgisprocess-deprecated
@@ -26,3 +30,33 @@ qgis_output <- function(x, which) {
     qgis_error_output_does_not_exist(x, which)
   }
 }
+
+
+
+#' @rdname qgisprocess-deprecated
+#' @export
+#' @keywords internal
+qgis_result_single <- function(x, what) {
+  .Deprecated("qgis_extract_output")
+  # Limit result to elements that match class
+  if (!missing(what)) {
+    x <- x[vapply(x, inherits, what, FUN.VALUE = logical(1))]
+    if (length(x) == 0L) {
+      abort(
+        paste(
+          "Can't extract object from result: zero outputs of type",
+          paste(what, collapse = " or ")
+        )
+      )
+    }
+  }
+
+  # By default, take the first element named as output or OUTPUT.
+  # Otherwise, take the first element that matches class.
+  result <- x[grepl("^(output|OUTPUT)$", names(x))][1][[1]]
+  if (is.null(result)) result <- x[[1]]
+  result
+}
+
+
+
