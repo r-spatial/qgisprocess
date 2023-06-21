d542dfee4675df890f30d6484cfc808619370f8c
florisvdh <floris.vanderhaeghe@inbo.be>
2023-06-21 15:33:35 +0200

    test-compat-sf.R: reorder tests

--- tests/testthat/test-compat-sf.R
+++ tests/testthat/test-compat-sf.R
@@ -1,19 +1,3 @@
-test_that("sf argument coercers work", {
-  skip_if_not_installed("sf")
-  sf_obj <- sf::read_sf(system.file("shape/nc.shp", package = "sf"))
-  expect_error(
-    as_qgis_argument(sf_obj),
-    "Can't convert 'sf' object"
-  )
-
-  tmp_file <- expect_match(
-    as_qgis_argument(sf_obj, qgis_argument_spec(qgis_type = "layer")),
-    "\\.gpkg$"
-  )
-  expect_s3_class(tmp_file, "qgis_tempfile_arg")
-  unlink(tmp_file)
-})
-
 test_that("sf result coercers work", {
   skip_if_not_installed("sf")
   skip_if_not(has_qgis())
@@ -53,6 +37,22 @@ test_that("sf result coercers work", {
   result$OUTPUT <- NULL
   unlink("llbuffer.gpkg")
   expect_error(sf::st_as_sf(result), "Can't extract object.")
+})
+
+test_that("sf argument coercers work", {
+  skip_if_not_installed("sf")
+  sf_obj <- sf::read_sf(system.file("shape/nc.shp", package = "sf"))
+  expect_error(
+    as_qgis_argument(sf_obj),
+    "Can't convert 'sf' object"
+  )
+
+  tmp_file <- expect_match(
+    as_qgis_argument(sf_obj, qgis_argument_spec(qgis_type = "layer")),
+    "\\.gpkg$"
+  )
+  expect_s3_class(tmp_file, "qgis_tempfile_arg")
+  unlink(tmp_file)
 })
 
 test_that("sf crs work", {
