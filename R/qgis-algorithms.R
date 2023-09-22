d260b7598eb0368d563df2492624fae7ed571c7c
florisvdh <floris.vanderhaeghe@inbo.be>
2023-09-22 13:07:14 +0200

    qgis_query_algorithms(): print stderr from qgis_process in R *
    qgis_query_algorithms() is not run often, and printing qgis_process's stderr here
    may help in debugging if needed.

--- R/qgis-algorithms.R
+++ R/qgis-algorithms.R
@@ -79,6 +79,14 @@ assert_qgis_algorithm <- function(algorithm) {
 qgis_query_algorithms <- function(quiet = FALSE) {
   if (qgis_using_json_output()) {
     result <- qgis_run(args = c("list", "--json"), encoding = "UTF-8")
+    if (nchar(result$stderr) > 0L) {
+      message(
+        "\nStandard error message emitted by the 'qgis_process' command ",
+        "(non-fatal at this stage):\n",
+        result$stderr,
+        "\n"
+      )
+    }
     result_parsed <- jsonlite::fromJSON(result$stdout)
 
     providers_ptype <- tibble::tibble(
