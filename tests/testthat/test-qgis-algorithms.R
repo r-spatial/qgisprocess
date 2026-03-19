bf0af6604885965eedceb35d9c8aed94fb3e8913
florisvdh <floris.vanderhaeghe@inbo.be>
2026-03-19 08:59:46 +0100

    test-qgis-algorithms.R: support both output methods in qgis_search_alg

--- tests/testthat/test-qgis-algorithms.R
+++ tests/testthat/test-qgis-algorithms.R
@@ -63,16 +63,36 @@ test_that("qgis_search_algorithms() works", {
   skip_if_not(has_qgis())
   expect_error(qgis_search_algorithms(), "at least one of the arguments")
   expect_error(qgis_search_algorithms(algorithm = 3))
-  res1 <- qgis_search_algorithms(
-    algorithm = "point.*line",
-    provider = "^native$",
-    group = "geometry"
-  )
+  if (qgis_using_json_output()) {
+    expect_no_message(
+      res1 <- qgis_search_algorithms(
+        algorithm = "point.*line",
+        provider = "^native$",
+        group = "geometry"
+      )
+    )
+  } else {
+    expect_message(
+      res1 <- qgis_search_algorithms(
+        algorithm = "point.*line",
+        provider = "^native$",
+        group = "geometry"
+      ),
+      "Ignoring"
+    )
+  }
   expect_s3_class(res1, "data.frame")
-  expect_identical(
-    colnames(res1),
-    c("provider", "provider_title", "group", "algorithm", "algorithm_title")
-  )
+  if (qgis_using_json_output()) {
+    expect_identical(
+      colnames(res1),
+      c("provider", "provider_title", "group", "algorithm", "algorithm_title")
+    )
+  } else {
+    expect_identical(
+      colnames(res1),
+      c("provider", "provider_title", "algorithm", "algorithm_title")
+    )
+  }
   expect_gt(nrow(res1), 0L)
   res2 <- qgis_search_algorithms(algorithm = "point.*line")
   expect_gt(nrow(res2), nrow(res1))
